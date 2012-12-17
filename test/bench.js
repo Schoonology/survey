9a525cabfe308fce03cdb243ff11dbf66c7291f2
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2012-12-17 15:11:55 -0800

    Added a benchmark for Survey. Meta much?

---
+++ test/bench.js
@@ -0,0 +1,11 @@
+module.exports = {
+    '1ms': function(done) {
+        setTimeout(done, 1);
+    },
+    '10ms': function(done) {
+        setTimeout(done, 10);
+    },
+    '100ms': function(done) {
+        setTimeout(done, 100);
+    }
+};
